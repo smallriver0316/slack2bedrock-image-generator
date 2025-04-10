79d507bdd489fbec62e15241d629a0345110fef2
smallriver0316 <stream0316@gmail.com>
2025-04-11 06:53:26 +0900

    Succeeded to connect with Slack

--- lambda/publisher/src/index.py
+++ lambda/publisher/src/index.py
@@ -1,10 +1,10 @@
-# from slack_bolt import App
-# from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 import boto3
 import json
 import logging
 import os
-# import re
+import re
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 
 
 logger = logging.getLogger(__name__)
@@ -12,45 +12,30 @@ logger.setLevel(logging.INFO)
 
 sqs = boto3.client("sqs")
 
-# app = App(
-#     token=os.environ.get("SLACK_BOT_TOKEN", ""),
-#     signing_secret=os.environ.get("SLACK_SIGNING_SECRET", ""),
-#     process_before_response=True)
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN", ""),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET", ""),
+    process_before_response=True)
 
 
-# @app.event("app_mention")
-# def handle_app_mention_events(event, say):
-#     logger.info(event)
-#     # say("What's up?")
-
-#     channel_id = event.get("channel", "")
-#     input_text = re.sub("<@.+>", "", event.get("text", "")).strip()
-
-#     sqs.send_message(
-#         QueueUrl=os.environ.get("SQS_QUEUE_URL"),
-#         MessageBody=json.dumps({
-#             "channel_id": channel_id,
-#             "input_text": input_text
-#         })
-#     )
-
-
-# def handler(event, context):
-#     logger.info(event)
-#     slack_handler = SlackRequestHandler(app=app)
-#     return slack_handler.handle(event, context)
-
-def handler(event, context):
+@app.event("app_mention")
+def handle_app_mention_events(event, say):
     logger.info(event)
+    say("What's up?")
+
+    channel_id = event.get("channel", "")
+    input_text = re.sub("<@.+>", "", event.get("text", "")).strip()
 
     sqs.send_message(
         QueueUrl=os.environ.get("SQS_QUEUE_URL"),
-        MessageBody=json.dumps(event)
+        MessageBody=json.dumps({
+            "channel_id": channel_id,
+            "input_text": input_text
+        })
     )
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello from publisher!"
-        })
-    }
+
+def handler(event, context):
+    logger.info(event)
+    slack_handler = SlackRequestHandler(app=app)
+    return slack_handler.handle(event, context)
